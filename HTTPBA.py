6e05e3bb732413121ce3590dc0178a0c29bf21b8
Clear <wjddkdml1124@naver.com>
2017-01-18 06:47:02 +0900

    recall the githubAPI and save the API in the textfile

---
+++ HTTPBA.py
@@ -0,0 +1,12 @@
+from urllib.request import *
+users = input("What is your username? ")
+passwd = input("password? ")
+auth_handler = HTTPBasicAuthHandler()
+openfile = open("/home/clear/Git/CommitCompute/example4.txt",'w')
+urlu = "http://api.github.com/users/"+ users + "/received_events"
+auth_handler.add_password(None, urlu, users,passwd )
+opener = build_opener(auth_handler)
+install_opener(opener)
+read = opener.open(urlu).read()
+openfile.write(str(read))
+openfile.close()
