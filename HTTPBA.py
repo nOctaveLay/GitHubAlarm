fbb33d008869de529735f50c2929f1f09e7bef45
Clear <wjddkdml1124@naver.com>
2017-01-19 19:02:46 +0900

    HTTP file, exactly api get.

--- HTTPBA.py
+++ HTTPBA.py
@@ -1,12 +1,13 @@
 from urllib.request import *
-users = input("What is your username? ")
-passwd = input("password? ")
-auth_handler = HTTPBasicAuthHandler()
-openfile = open("<filename>",'w')
-urlu = "http://api.github.com/users/"+ users + "/received_events"
-auth_handler.add_password(None, urlu, users,passwd )
-opener = build_opener(auth_handler)
-install_opener(opener)
-read = opener.open(urlu).read()
-openfile.write(str(read))
-openfile.close()
+def pass_internetfile():
+	users = input("What is your username? ")
+	passwd = input("password? ")
+	auth_handler = HTTPBasicAuthHandler()
+	openfile = open("internetfile.txt",'w')
+	urlu = "http://api.github.com/users/"+ users + "/received_events"
+	auth_handler.add_password(None, urlu, users,passwd )
+	opener = build_opener(auth_handler)
+	install_opener(opener)
+	read = opener.open(urlu).read()
+	openfile.write(str(read))
+	openfile.close()
