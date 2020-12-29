f33eb1b70ed9ecfccc5661f66d7ccd9b99828c66
nOctaveLay <wjddkdml1124@naver.com>
2020-12-29 10:45:09 +0900

    Revert "GitAlarm의 폴더가 있을 필요가 없다."
    This reverts commit 8d5f17465e782f47c96c9ed425ca163467e0c01c.

--- GitAlarm/UrlTreat.py
+++ GitAlarm/UrlTreat.py
@@ -4,16 +4,18 @@ from urllib.request import HTTPBasicAuthHandler,build_opener,install_opener,urlo
 
 __all__ = ['pass_internetfile_plus_password','read_data_from_url']
 
-def pass_internetfile_plus_password(url,users,password):
+def pass_internetfile_plus_password():
 	#receive the user's name & password
-	# users = input("What is your username? ")
-	# passwd = getpass("password? ")
+	users = input("What is your username? ")
+	passwd = getpass("password? ")
 
 	#make HTTPBasicAuthHandler, object => it can add the password. 
 	auth_handler = HTTPBasicAuthHandler()
 
+	urlu = f"http://api.github.com/users/{users}/received_events"
+	
 	# add password in url_u by using users and password
-	auth_handler.add_password(None, url, users,password )
+	auth_handler.add_password(None, urlu, users,passwd )
 	
 	#open the auth_handler.
 	opener = build_opener(auth_handler)
@@ -22,10 +24,10 @@ def pass_internetfile_plus_password(url,users,password):
 	install_opener(opener)
 	
 	#read the server's data
-	read = opener.open(url).read().decode('utf-8')
+	read = opener.open(urlu).read()
 
 	#file open, that is written user's received_events
-	return read
+	return read.decode('utf-8')
 
 def read_data_from_url(url):
 	'''
